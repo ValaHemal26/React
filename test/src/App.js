5f3b2aeb978d2526e8216ecaf2c0be73f5e3db19
Hemal <valahemal26@gmail.com>
2025-12-01 16:42:35 +0530

    Added new components and updated layout

--- test/src/App.js
+++ test/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='wrapper'>
+      <div id='header'>
+        <h1>Header</h1>
+        
+      </div>
+      <div className='main'>
+        <div className='sidebar'>
+          sidebar
+        </div>
+        <div className='content'>
+            content
+        </div>
+      </div>
+      <div id='footer'>
+        <h1>Footer</h1>
+      </div>
     </div>
   );
 }
